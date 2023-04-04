18299f2840319028c6ab2ada386c164c78c10d9c
John Wanjema <jonwanjema@gmail.com>
2023-04-04 10:23:30 -0700

    search for an element in a 2d array

---
+++ arrays/searchIn2DArray.py
@@ -0,0 +1,19 @@
+def search2DArray(arr,target):
+    """
+       Search for an element in a 2d array
+    """
+
+    #if array length equals zero
+    if(len(arr) == 0):
+        return -1
+
+
+    for i in arr:
+        for j in i:
+           if(j == target):
+                return True
+    
+    return -1
+
+
+print(search2DArray([{23,4,1},{18,12,3,8},{13,89}],18))
