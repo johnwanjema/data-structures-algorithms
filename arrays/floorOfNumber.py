ed4fbf388544ad0ab148aaab1b856db0f6ef0980
John Wanjema <jonwanjema@gmail.com>
2023-09-20 16:37:19 -0700

    add kata for finding ceilling and floor of a number in an array

---
+++ arrays/floorOfNumber.py
@@ -0,0 +1,25 @@
+def CeillingOfANumber(arr,target):
+    """
+       Ceilling of a number is a number smaller than or equal to target element
+    """
+    print(target)
+    print(arr)
+    #if array length equals zero
+    if(len(arr) == 0):
+        return -1
+
+    start = 0
+    end = len(arr) - 1
+
+    while start <= end:
+        mid = start + (end - start ) // 2
+
+        if(target < arr[mid]):
+            end = mid - 1
+        elif (target > arr[mid]):
+            start = mid + 1
+
+    
+    return arr[end]
+
+print(CeillingOfANumber([2,3,5,9,14,16,18],15))
