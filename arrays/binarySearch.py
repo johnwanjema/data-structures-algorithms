7ba3d18f14aa8d15ff0b3ab97ef74639c9b74dc8
John Wanjema <jonwanjema@gmail.com>
2023-09-20 16:00:42 -0700

    add kata for binary search

---
+++ arrays/binarySearch.py
@@ -0,0 +1,25 @@
+def binarySearch(arr,target):
+    """
+       Binary search one dimensional array
+    """
+
+    #if array length equals zero
+    if(len(arr) == 0):
+        return -1
+
+    start = 0
+    end = len(arr) - 1
+
+    while start <= end:
+        mid = start + (end - start ) // 2
+
+        if(target < arr[mid]):
+            end = mid - 1
+        elif (target > arr[mid]):
+            start = mid + 1
+        else:
+            return mid
+    
+    return -1
+
+print(binarySearch([1,4,14 ,78,768],14))
