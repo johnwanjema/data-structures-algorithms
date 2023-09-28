4967002db76aa01ebbbcb4d73fcc1cfcca5151e5
John Wanjema <jonwanjema@gmail.com>
2023-09-28 10:24:29 -0700

    add kata for guess number higher or lower

---
+++ arrays/guessNumberHigherOrLower.py
@@ -0,0 +1,27 @@
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if num is higher than the picked number
+#          1 if num is lower than the picked number
+#          otherwise return 0
+# def guess(num):
+
+class Solution(object):
+    def guessNumber(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        start = 1
+        end = n
+
+        while start <= end:
+            mid = (start + end) // 2
+
+            if(guess(mid) == 0):
+                return mid
+            elif(guess(mid) == -1):
+                end = mid -1
+            else:
+                start = mid + 1    
+        
+        return -1
