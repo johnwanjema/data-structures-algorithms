770860f07fde77034ba912e30d3b3fb1e38b153b
John Wanjema <jonwanjema@gmail.com>
2023-10-02 14:50:59 -0700

    add kata for search 2d array

--- arrays/searchIn2DArray.py
+++ arrays/searchIn2DArray.py
@@ -1,19 +1,45 @@
 def search2DArray(arr,target):
     """
-      Search for an element in a 2d array
+        Search for an element in a 2d array
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
     """
+    # n=len(matrix)
+    # m=len(matrix[0])
+
+    # Linear search
+    # for i in range(n):
+    #     for j in range(m):
+    #         if matrix[i][j]==target:
+    #             return True
+
+    # return False     
 
     #if array length equals zero
     if(len(arr) == 0):
         return -1
 
+    row = 0
+    # n x m matrix
+    col = len(matrix[0]) - 1
+    # n x n matrix
+    col = len(matrix) - 1
 
-    for i in arr:
-        for j in i:
-           if(j == target):
-                return True
+
+    while row < len(matrix) and col >= 0:
+        if(matrix[row][col] == target):
+            return True
+        
+        if(matrix[row][col] < target):
+            row += 1
+        else:
+            col -= 1
     
-    return -1
+    return False  
 
 
-print(search2DArray([{23,4,1},{18,12,3,8},{13,89}],18))
+
+
+# print(search2DArray([[10,20,30],[15,25,35],[28,29,37]],25))
+print(search2DArray([[1,3,5,7],[10,11,16,20],[23,30,34,60]],300))
